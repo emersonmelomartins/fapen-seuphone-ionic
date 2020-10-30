e58d027303b2f8837172cac8d739d20b7bd67dde
Emerson Melo Martins <emerson25xd@gmail.com>
2020-10-29 21:38:21 -0300

    Adicionado model e service de usuario

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,3 +1,18 @@
-interface User {
-  nome: string;
+export interface User {
+  "idLogin": number,
+  "pessoa": Pessoa,
+  "login": string,
+  "senha": string,
+  "email": string,
+  "caminhoFoto": string,
+  "inativo": boolean,
+}
+interface Pessoa {
+  "idPessoa": number,
+  "nome": string,
+  "cpf": string,
+  "dtNascimento": string,
+  "sexo": string,
+  "celular": string,
+  "telefone": string
 }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../models/User';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  doLogin() {
+    return this.http.get<User>(`http://localhost:8080/api/usuarios/login`);
+  }
+
+  createUser() {
+    return this.http.get<User>(`http://localhost:8080/api/usuarios/login`);
+  }
 }
