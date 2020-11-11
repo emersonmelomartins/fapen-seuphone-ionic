eb183641bc69b9b8ea64a412d2abe3526e6e5c6e
Emerson Melo Martins <emerson25xd@gmail.com>
2020-11-10 23:26:16 -0300

    Adiçao de termos com validaçao, ajuste valores

--- src/app/pages/terms/terms.page.ts
+++ src/app/pages/terms/terms.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-terms',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TermsPage implements OnInit {
 
-  constructor() { }
+  public formGroup: FormGroup;
+
+  public options = [
+    {
+      label: 'Aceitar',
+      value: true
+    },
+    {
+      label: 'Recusar',
+      value: false
+    }
+  ];
+
+  public selectedOption: boolean = false;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.formGroup = formBuilder.group({
+      radioOption: [
+        this.selectedOption,
+        Validators.compose([Validators.requiredTrue]),
+      ],
+    });
+
+  }
 
   ngOnInit() {
+  }
+
+  radioChange(event) {
+    this.selectedOption = event.target.value;
   }
 
 }
